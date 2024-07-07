cc29a215204982de49aa7d2e7ee5f0873d84f7e0
kobesar <klobby19@gmail.com>
2024-07-06 23:00:05 -0700

    user auth

--- src/components/Todos.vue
+++ src/components/Todos.vue
@@ -25,11 +25,16 @@ function createTodo() {
     listTodos();
   });
 }
+  
+function deleteTodo(id: string) {
+    client.models.Todo.delete({ id })
+  }
     
 // fetch todos when the component is mounted
  onMounted(() => {
   listTodos();
 });
+
 
 </script>
 
@@ -40,7 +45,9 @@ function createTodo() {
     <ul>
       <li 
         v-for="todo in todos" 
-        :key="todo.id">
+        :key="todo.id"
+        @click="deleteTodo(todo.id)">
+        
         {{ todo.content }}
       </li>
     </ul>
